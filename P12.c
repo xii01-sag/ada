1adcf4ad6383b3db86ca363f90b5783280654a00
xii01 <rahulsagarxii01@gmail.com>
2026-03-09 18:11:39 +0530

    Create P12.c

---
+++ P12.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+void printSolution(int **board, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%s ", board[i][j] ? "Q" : "#");
+        }
+        printf("\n");
+    }
+}
+bool isSafe(int **board, int N, int row, int col)
+{
+    int i, j;
+    for (i = 0; i < col; i++)
+    {
+        if (board[row][i])
+        {
+            return false;
+        }
+    }
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+    {
+        if (board[i][j])
+        {
+            return false;
+        }
+    }
+    for (i = row, j = col; j >= 0 && i < N; i++, j--)
+    {
+        if (board[i][j])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+bool solveNQUtil(int **board, int N, int col)
+{
+    if (col >= N)
+    {
+        return true;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        if (isSafe(board, N, i, col))
+        {
+            board[i][col] = 1;
+            if (solveNQUtil(board, N, col + 1))
+            {
+                return true;
+            }
+            board[i][col] = 0;
+        }
+    }
+    return false;
+}
+bool solveNQ(int N)
+{
+    int **board = (int **)malloc(N * sizeof(int *));
+    for (int i = 0; i < N; i++)
+    {
+        board[i] = (int *)malloc(N * sizeof(int));
+        for (int j = 0; j < N; j++)
+        {
+            board[i][j] = 0;
+        }
+    }
+    if (!solveNQUtil(board, N, 0))
+    {
+        printf("Solution does not exist\n");
+        for (int i = 0; i < N; i++)
+        {
+            free(board[i]);
+        }
+        free(board);
+        return false;
+    }
+    printSolution(board, N);
+    for (int i = 0; i < N; i++)
+    {
+        free(board[i]);
+    }
+    free(board);
+    return true;
+}
+int main()
+{
+    int N;
+    printf("Enter the number of queens: ");
+    scanf("%d", &N);
+    solveNQ(N);
+    return 0;
+}
