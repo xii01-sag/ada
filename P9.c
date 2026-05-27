95ca1bde2bbcb9a69cd16915e660d208d827c77e
xii01 <rahulsagarxii01@gmail.com>
2026-05-27 16:09:02 +0530

    P9.c

--- P9.c
+++ P9.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-void selectionSort(int arr[], int n)
-{
+
+void selectionSort(int arr[], int n) {
     int i, j, min_idx;
-    for (i = 0; i < n-1; i++)
-    {
+    for (i = 0; i < n-1; i++) {
         min_idx = i;
-        for (j = i+1; j < n; j++)
-        {
-            if (arr[j] < arr[min_idx])
-            {
+        for (j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
                 min_idx = j;
             }
         }
@@ -19,35 +16,55 @@ void selectionSort(int arr[], int n)
         arr[i] = temp;
     }
 }
-void generateRandomNumbers(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
+
+void generateRandomNumbers(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
         arr[i] = rand() % 10000;
     }
 }
-int main()
-{
-    int n;
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
-    if (n <= 5000)
-    {
-        printf("Please enter a value greater than 5000\n");
+
+int main() {
+    FILE *fp;
+    fp = fopen("selection_data.txt", "w");
+    if (fp == NULL) {
         return 1;
     }
-    int *arr = (int *)malloc(n * sizeof(int));
-    if (arr == NULL)
-    {
-        printf("Memory allocation failed\n");
-        return 1;
+    
+    int n, i;
+    clock_t start, end;
+    double time_taken;
+
+    printf("\nSelection Sort Performance Analysis\n");
+    printf("--------------------------------------\n");
+    srand(time(NULL));
+
+    for (n = 5000; n <= 50000; n += 5000) {
+        int *arr = (int *)malloc(n * sizeof(int));
+        if (arr == NULL) {
+            fclose(fp);
+            return 1;
+        }
+        generateRandomNumbers(arr, n);
+        start = clock();
+        selectionSort(arr, n);
+        end = clock();
+        time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+        printf("n = %d \t Time = %f seconds\n", n, time_taken);
+        fprintf(fp, "%d %f\n", n, time_taken);
+        free(arr);
     }
-    generateRandomNumbers(arr, n);
-    clock_t start = clock();
-    selectionSort(arr, n);
-    clock_t end = clock();
-    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Time taken to sort %d elements: %f seconds\n", n, time_taken);
-    free(arr);
+
+    fclose(fp);
+
+    FILE *gnuplot = popen("gnuplot -persist", "w");
+    if (gnuplot != NULL) {
+        fprintf(gnuplot, "set title 'Selection Sort Time Complexity'\n");
+        fprintf(gnuplot, "set xlabel 'Number of Elements (n)'\n");
+        fprintf(gnuplot, "set ylabel 'Time Taken (seconds)'\n");
+        fprintf(gnuplot, "set grid\n");
+        fprintf(gnuplot, "plot 'selection_data.txt' using 1:2 with linespoints linewidth 2 title 'Selection Sort'\n");
+        pclose(gnuplot);
+    }
+
     return 0;
 }
