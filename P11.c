1aae4c4143ad410863036a340aadcadf644a2094
xii01 <rahulsagarxii01@gmail.com>
2026-05-27 16:06:28 +0530

    P11.c

--- P11.c
+++ P11.c
@@ -1,89 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 void merge(int arr[], int left, int mid, int right)
 {
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    int *L = (int *)malloc(n1 * sizeof(int));
-    int *R = (int *)malloc(n2 * sizeof(int));
-    for (i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-    i = 0;
-    j = 0;
-    k = left;
-    while (i < n1 && j < n2)
+    int i = left;
+    int j = mid + 1;
+    int k = 0;
+    int *temp = (int *)malloc((right - left + 1) * sizeof(int));
+    if (temp == NULL) return;
+
+    while (i <= mid && j <= right)
     {
-        if (L[i] <= R[j])
-        {
-            arr[k] = L[i];
-            i++;
-        }
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
         else
-        {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
+            temp[k++] = arr[j++];
     }
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-    free(L);
-    free(R);
+    while (i <= mid)
+        temp[k++] = arr[i++];
+    while (j <= right)
+        temp[k++] = arr[j++];
+    for (i = left, k = 0; i <= right; i++, k++)
+        arr[i] = temp[k];
+        
+    free(temp);
 }
+
 void mergeSort(int arr[], int left, int right)
 {
     if (left < right)
     {
-        int mid = left + (right - left) / 2;
+        int mid = (left + right) / 2;
         mergeSort(arr, left, mid);
         mergeSort(arr, mid + 1, right);
         merge(arr, left, mid, right);
     }
 }
-void generateRandomArray(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-        arr[i] = rand() % 100000;
-}
+
 int main()
 {
-    int n;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-    if (n <= 5000)
+    FILE *fp;
+    fp = fopen("merge_data.txt", "w");
+    if (fp == NULL)
     {
-        printf("Please enter a value greater than 5000\n");
         return 1;
     }
-    int *arr = (int *)malloc(n * sizeof(int));
-    if (arr == NULL)
+
+    int n, i;
+    clock_t start, end;
+    double time_taken;
+
+    printf("\nMerge Sort Performance Analysis\n");
+    printf("-----------------------------------\n");
+    srand(time(NULL));
+
+    for (n = 5000; n <= 50000; n += 5000)
     {
-        printf("Memory allocation failed\n");
-        return 1;
-    }
-    generateRandomArray(arr, n);
-    clock_t start = clock();
-    for (int i = 0; i < 1000; i++)
-    {
+        int *arr = (int *)malloc(n * sizeof(int));
+        if (arr == NULL)
+        {
+            fclose(fp);
+            return 1;
+        }
+
+        for (i = 0; i < n; i++)
+        {
+            arr[i] = rand() % 100000;
+        }
+
+        start = clock();
         mergeSort(arr, 0, n - 1);
+        end = clock();
+
+        time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+        printf("n = %d \t Time = %f seconds\n", n, time_taken);
+
+        fprintf(fp, "%d %f\n", n, time_taken);
+        free(arr);
     }
-    clock_t end = clock();
-    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC / 1000.0;
-    printf("Time taken to sort %d elements: %f seconds\n", n, time_taken);
-    free(arr);
+    fclose(fp);
+
+    FILE *gnuplot = popen("gnuplot -persist", "w");
+    if (gnuplot != NULL)
+    {
+        fprintf(gnuplot, "set title 'Merge Sort Time Complexity'\n");
+        fprintf(gnuplot, "set xlabel 'Number of Elements (n)'\n");
+        fprintf(gnuplot, "set ylabel 'Time Taken (seconds)'\n");
+        fprintf(gnuplot, "set grid\n");
+        fprintf(gnuplot, "plot 'merge_data.txt' using 1:2 with linespoints linewidth 2 title 'Merge Sort'\n");
+        pclose(gnuplot);
+    }
+
     return 0;
 }
